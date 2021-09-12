f5eb3623ce00316719709eb4e2d2f13fb66703d9
Rolandas Seputis <vi.kvadrantas@gmail.com>
2021-09-12 21:45:37 +0300

    event loop

---
+++ 103-event-loop-2.js
@@ -0,0 +1,59 @@
+const fs = require("fs");
+ 
+console.log("pradzia");
+ 
+let allData = "";
+fs.readFile("a.txt", {
+ encoding: "utf-8"
+}, function(err, data) {
+ if (err) {
+ console.log("nepavyko perskaityti failo");
+ console.log(err);
+ return;
+ }
+ console.log("perskaiciau faila a.txt");
+ allData += data;
+});
+
+fs.readFile("b.txt", {
+ encoding: "utf-8"
+}, function(err, data) {
+ if (err) {
+ console.log("nepavyko perskaityti failo");
+ console.log(err);
+ return;
+ }
+ console.log("perskaiciau faila b.txt");
+ allData += data;
+});
+
+fs.readFile("c.txt", {
+ encoding: "utf-8"
+}, function(err, data) {
+ if (err) {
+ console.log("nepavyko perskaityti failo");
+ console.log(err);
+ return;
+ }
+ console.log("perskaiciau faila c.txt");
+ allData += data;
+});
+
+fs.readFile("d.txt", {
+ encoding: "utf-8"
+}, function(err, data) {
+ if (err) {
+ console.log("nepavyko perskaityti failo");
+ console.log(err);
+ return;
+ }
+ console.log("perskaiciau faila d.txt");
+ allData += data;
+});
+ 
+console.log("allData:", allData);
+ 
+setTimeout(function() {
+console.log(allData);
+}, 1000);
+console.log("pabaiga");
